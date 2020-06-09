61dcb1c129489046c383d02f32a8e0f9716c9dc7
jorgealziro <jorge.ardcs@gmail.com>
2020-06-08 23:07:42 -0300

    added basics of web server code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+const port = process.env.port || 4000;
+
+app.get('/', (req, res) => res.send('Hello World'));
+app.listen(4000, () => console.log(`Server running at http://localhost:${port}`));
