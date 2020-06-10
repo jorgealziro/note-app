793db53aa2f00e30e2bc8e4dd69f00ec3f19672a
jorgealziro <jorge.ardcs@gmail.com>
2020-06-09 22:48:15 -0300

    Added graphQL queries and resolvers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import {ApolloServer, gql} from 'apollo-server-express';
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+    `;
+const resolvers = {
+    Query: {
+        hello: () => 'Hello World'
+    }
+};
+
 const app = express();
 const port = process.env.port || 4000;
 
+const server = new ApolloServer({ typeDefs, resolvers});
+server.applyMiddleware({ app, path: '/api'});
+
 app.get('/', (req, res) => res.send('Hello World'));
-app.listen(4000, () => console.log(`Server running at http://localhost:${port}`));
+app.listen({port}, () =>
+    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`));
