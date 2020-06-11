4df9c762e59f6cc2e86b1094e1fac326991eaad5
jorgealziro <jorge.ardcs@gmail.com>
2020-06-10 21:25:55 -0300

    Implemented GQL query and mutation resolvers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import express from 'express';
 import {ApolloServer, gql} from 'apollo-server-express';
 
+let notes = [
+    {id: '1', content: 'This is a note', author: 'JEN'},
+    {id: '2', content: 'This is another note', author: 'Aniston'},
+    {id: '3', content: 'Banana', author: 'Kong'}
+];
+
 const typeDefs = gql`
     type Query {
-        hello: String
+        hello: String!
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+
+    type Mutation {
+        newNote(content: String!): Note!
     }
     `;
 const resolvers = {
     Query: {
-        hello: () => 'Hello World'
+        hello: () => 'Hello World',
+        notes: () => notes,
+        note: (parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Michael Scott'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
     }
 };
 
@@ -20,4 +53,5 @@ server.applyMiddleware({ app, path: '/api'});
 
 app.get('/', (req, res) => res.send('Hello World'));
 app.listen({port}, () =>
-    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`));
+    console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`)
+    );
