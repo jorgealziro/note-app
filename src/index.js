b0a6fbfc2226a26bbafd598e13dc97cf1f5381ea
jorgealziro <jorge.ardcs@gmail.com>
2020-06-13 21:33:20 -0300

    Added User to the resolver context

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
+import jwt from 'jsonwebtoken';
 require('dotenv').config();
 
 const resolvers = require('./resolvers');
@@ -9,14 +10,28 @@ const typeDefs = require('./schema');
 const port = process.env.port || 4000;
 const DB_HOST = process.env.DB_HOST;
 
+const getUser = token => {
+    if (token) {
+        try {
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            throw new Error('Session Invalid');
+        }
+    }
+};
+
+
 const app = express();
 db.connect(DB_HOST);
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => {
-        return { models };
+    context: ({req}) => {
+        const token = req.headers.authorization;
+        const user = getUser(token);
+        console.log(user)
+        return { models, user };
     }
 });
 server.applyMiddleware({ app, path: '/api'});
