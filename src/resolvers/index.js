815173f70f28c51a5da21469254ccbcd72426d45
jorgealziro <jorge.ardcs@gmail.com>
2020-06-26 00:20:51 -0300

    Added nested queries capability

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,10 +1,14 @@
 const Query = require('./query');
 const Mutation = require('./mutation');
+import Note from './note';
+import User from './user';
 const {GraphQLDateTime} = require('graphql-iso-date');
 
 module.exports = {
     Query,
     Mutation,
+    Note,
+    User,
     DateTime: GraphQLDateTime
 };
 
